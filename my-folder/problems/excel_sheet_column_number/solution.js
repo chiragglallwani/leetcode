601a914598c782b5048296a849ac7200d8659d9f
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-03-30 20:24:27 +0000

    Sync LeetCode submission - Excel Sheet Column Number (javascript)

---
+++ my-folder/problems/excel_sheet_column_number/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++)
+    {
+        result *= 26;
+        result += columnTitle[i].charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+    }
+    return result;
+};
