d32b93764a52b6e3453787a0d171d5deed532380
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-05-21 06:57:33 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (javascript)

---
+++ my-folder/problems/remove_duplicates_from_sorted_array/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 1;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i - 1] !== nums[i]){
+            nums[count++] = nums[i];
+        }
+    }
+    return count;
+};
