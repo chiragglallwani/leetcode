6cc7578d4f4d5a91a81a90954ce5a8dab16694db
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-05-22 04:01:35 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (javascript)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.js
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let left = 0; // Buy
-  let right = 1; // sell
-  let max_profit = 0;
-  while (right < prices.length) {
-    if (prices[left] < prices[right]) {
-      let profit = prices[right] - prices[left]; // our current profit
-
-      max_profit = Math.max(max_profit, profit);
-    } else {
-      left = right;
-    }
-    right++;
+  let left = 0, right = 1, maxProfit = 0;
+  while(right < prices.length){
+      if(prices[left] < prices[right]){
+          profit = prices[right] - prices[left];
+          maxProfit = Math.max(maxProfit, profit);
+      }else{
+          left = right;
+      }
+      right++;
   }
-  return max_profit;
+  return maxProfit
 };
