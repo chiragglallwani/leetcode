2620ea15ab07c77495ce8d656cc978af9b37de95
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-07-30 17:01:38 +0000

    Sync LeetCode submission - Binary Tree Right Side View (javascript)

---
+++ my-folder/problems/binary_tree_right_side_view/solution.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(!root) return [];
+    let queue = [root];
+    let output = [];
+    console.log(root);
+    while(queue.length){
+        let q = [];
+        let qLen = queue.length;
+        while(qLen--){
+            let node = queue.shift(root);
+            
+            if(node.left){
+                queue.push(node.left);
+            }
+            if(node.right){
+                queue.push(node.right);
+            }
+            q.push(node.val);
+        }
+        output.push(q.pop())
+    }
+    return output;
+};
