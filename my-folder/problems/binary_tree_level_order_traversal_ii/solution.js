7c42750a8a15f2b34329b239b3256e417f65b096
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-07-30 17:22:12 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal II (javascript)

---
+++ my-folder/problems/binary_tree_level_order_traversal_ii/solution.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if(!root) return [];
+    let queue = [root];
+    let output = [];
+    console.log(root);
+    while(queue.length){
+        let q = [];
+        let qLen = queue.length;
+        while(qLen--){
+            let node = queue.shift(root);
+            
+            if(node.left){
+                queue.push(node.left);
+            }
+            if(node.right){
+                queue.push(node.right);
+            }
+            q.push(node.val);
+        }
+        output.push(q)
+    }
+    output.reverse();
+    return output;
+};
